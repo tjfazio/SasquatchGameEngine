156f4d4a4840bdfc6f395aecf13a0468098948f0
tjfazio <tjfazio@users.noreply.github.com>
2017-01-24 00:09:47 -0800

    Win32 window boilerplate
    Added Win32 boilerplate for opening a window

--- code/win32_sasquatch.cpp
+++ code/win32_sasquatch.cpp
@@ -1,9 +1,100 @@
-#include <iostream>
-using namespace std;
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
 
-int main()
+static TCHAR szWindowClass[] = _T("sasquatch");
+static TCHAR szTitle[] = _T("Sasquatch Game Engine");
+
+LRESULT WndProc(HWND hwnd,  UINT uMsg,  WPARAM wParam,  LPARAM lParam);
+
+int WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = NULL;
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = NULL;
+
+    if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("Call to RegisterClassEx failed!"),
+            _T("Sasquatch"),
+            NULL
+        );
+        
+        return 1;
+    }
+
+    HWND hWnd = CreateWindow(
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 100,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+    
+    if (!hWnd)
+    {
+        MessageBox(NULL,
+            _T("Call to CreateWindow failed!"),
+            _T("Sasquatch"),
+            NULL
+        );
+
+        return 1;
+    }
+    
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+LRESULT WndProc(  
+  HWND hWnd,  
+  UINT uMsg,  
+  WPARAM wParam,  
+  LPARAM lParam  
+)
 {    
-    int x;
-    cout << "Hello World!" << endl;
-    cin >> x;
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+            break;
+    }
+
+    return 0;
 }

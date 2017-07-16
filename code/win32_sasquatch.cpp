dd542eef09c5026dad633a0bcd826b8457806c2a
tjfazio <anthony.t.fazio@gmail.com>
2017-07-15 23:29:08 -0700

    platform initialization functions
    * Moved some of the platform initialization code into functions
    * Disabled window resizing

--- code/win32_sasquatch.cpp
+++ code/win32_sasquatch.cpp
@@ -23,6 +23,9 @@
 global_variable TCHAR szWindowClass[] = _T("sasquatch");
 global_variable TCHAR szTitle[] = _T("Sasquatch Game Engine");
 
+const uint64_t Win32_PlatformMemorySize = KILOBYTES(64);
+const uint64_t Win32_GameStaticMemorySize = GIGABYTES(2);
+
 // TODO: move these structs to a .h file
 typedef struct tagBitmapBuffer {
     int32_t Height;
@@ -165,8 +168,54 @@ internal real32_t Win32_GetElapsedSeconds(LARGE_INTEGER lastTime)
     return elapsedSeconds;
 }
 
-const uint64_t Win32_PlatformMemorySize = KILOBYTES(64);
-const uint64_t Win32_GameStaticMemorySize = GIGABYTES(2);
+internal void Win32_InitializeSound(
+    Win32_SoundOutput **win32SoundOutputPtr, 
+    SGE_SoundBuffer **gameSoundBufferPtr,
+    SGE_GameClock *gameClock,
+    HWND hWnd)
+{        
+    uint8_t *platformMemory = (uint8_t *)VirtualAlloc(NULL, Win32_PlatformMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+    assert(platformMemory != NULL);
+    uint64_t platformMemoryNextByte = 0;
+
+    Win32_SoundOutput *win32SoundOutput = (Win32_SoundOutput *)(platformMemory + platformMemoryNextByte);
+    platformMemoryNextByte += sizeof(Win32_SoundOutput);
+
+    int32_t samplesPerFrame = (int32_t)(Win32_SoundSamplesPerSecond / gameClock->TargetFrameRate);
+    int32_t soundBufferSize = 4 * Win32_NumChannels * sizeof(sample_t) * samplesPerFrame;
+    Win32_InitializeDirectSound(win32SoundOutput, hWnd, Win32_SoundBufferSize, Win32_SoundSamplesPerSecond);
+    Win32_ClearSoundBuffer(win32SoundOutput);
+    win32SoundOutput->SecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
+
+    SGE_SoundBuffer *gameSoundBuffer = (SGE_SoundBuffer *)(platformMemory + platformMemoryNextByte);
+    platformMemoryNextByte += sizeof(SGE_SoundBuffer);
+
+    gameSoundBuffer->SamplesPerSecond = Win32_SoundSamplesPerSecond;
+    gameSoundBuffer->NumChannels = Win32_NumChannels;
+    gameSoundBuffer->BufferSize = soundBufferSize;
+    gameSoundBuffer->Memory = (sample_t *)(platformMemory + platformMemoryNextByte);
+    platformMemoryNextByte += soundBufferSize;
+    
+    assert(Win32_PlatformMemorySize > platformMemoryNextByte);
+
+    *win32SoundOutputPtr = win32SoundOutput;
+    *gameSoundBufferPtr = gameSoundBuffer;
+}
+
+internal SGE_GameState *Win32_InitializeGameState()
+{
+    uint8_t *gameStaticMemory = (uint8_t *)VirtualAlloc(NULL, Win32_GameStaticMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+    assert(gameStaticMemory != NULL);
+    assert(Win32_GameStaticMemorySize > sizeof(SGE_GameState));
+    SGE_GameState *gameState = (SGE_GameState *)gameStaticMemory;
+    // HACK: need a global pointer for updating the game controllers from Windows events 
+    g_GameState = gameState;
+
+    SGE_Init(gameState);
+    Win32_InitializeDefaultKeyboardMap(&gameState->Controllers[SGE_Keyboard]);
+
+    return gameState;
+}
 
 int WinMain(
     HINSTANCE hInstance,
@@ -204,10 +253,11 @@ int WinMain(
         return 1;
     }
 
+    DWORD windowStyle = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
     HWND hWnd = CreateWindow(
         szWindowClass,
         szTitle,
-        WS_OVERLAPPEDWINDOW,
+        windowStyle,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
         windowWidth,
@@ -244,48 +294,11 @@ int WinMain(
     // TODO: figure out why I need so much padding for sound to work
     gameClock.FrameVariationSeconds = (real32_t)(2.0 * gameClock.TargetFrameLengthSeconds);
 
-    // initialize sound
     Win32_SoundOutput *win32SoundOutput;
     SGE_SoundBuffer *gameSoundBuffer;
-    {        
-        uint8_t *platformMemory = (uint8_t *)VirtualAlloc(NULL, Win32_PlatformMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
-        assert(platformMemory != NULL);
-        uint64_t platformMemoryNextByte = 0;
+    Win32_InitializeSound(&win32SoundOutput, &gameSoundBuffer, &gameClock, hWnd);
 
-        win32SoundOutput = (Win32_SoundOutput *)(platformMemory + platformMemoryNextByte);
-        platformMemoryNextByte += sizeof(Win32_SoundOutput);
-
-        int32_t samplesPerFrame = (int32_t)(Win32_SoundSamplesPerSecond / gameClock.TargetFrameRate);
-        int32_t soundBufferSize = 4 * Win32_NumChannels * sizeof(sample_t) * samplesPerFrame;
-        Win32_InitializeDirectSound(win32SoundOutput, hWnd, Win32_SoundBufferSize, Win32_SoundSamplesPerSecond);
-        Win32_ClearSoundBuffer(win32SoundOutput);
-        win32SoundOutput->SecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
-
-        gameSoundBuffer = (SGE_SoundBuffer *)(platformMemory + platformMemoryNextByte);
-        platformMemoryNextByte += sizeof(SGE_SoundBuffer);
-
-        gameSoundBuffer->SamplesPerSecond = Win32_SoundSamplesPerSecond;
-        gameSoundBuffer->NumChannels = Win32_NumChannels;
-        gameSoundBuffer->BufferSize = soundBufferSize;
-        gameSoundBuffer->Memory = (sample_t *)(platformMemory + platformMemoryNextByte);
-        platformMemoryNextByte += soundBufferSize;
-        
-        assert(Win32_PlatformMemorySize > platformMemoryNextByte);
-    }
-
-    // initialize game state
-    SGE_GameState *gameState;
-    {
-        uint8_t *gameStaticMemory = (uint8_t *)VirtualAlloc(NULL, Win32_GameStaticMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
-        assert(gameStaticMemory != NULL);
-        assert(Win32_GameStaticMemorySize > sizeof(SGE_GameState));
-        gameState = (SGE_GameState *)gameStaticMemory;
-        // HACK: need a global pointer for updating the game controllers from Windows events 
-        g_GameState = gameState;
-
-        SGE_Init(gameState);
-        Win32_InitializeDefaultKeyboardMap(&gameState->Controllers[SGE_Keyboard]);
-    }
+    SGE_GameState *gameState = Win32_InitializeGameState();
 
     // Windows will clean this HDC up when the game exits
     HDC deviceContext = GetDC(hWnd);
